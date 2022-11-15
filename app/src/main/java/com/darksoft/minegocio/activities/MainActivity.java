bdae4899ba3fd55c22f0925f978b54d9d719985a
Juan  Montero <juan.montero@devyou.co>
2022-11-15 16:56:14 -0400

    Nov 15 | Cierre de Sesion

--- app/src/main/java/com/darksoft/minegocio/activities/MainActivity.java
+++ app/src/main/java/com/darksoft/minegocio/activities/MainActivity.java
@@ -1,51 +1,23 @@
 package com.darksoft.minegocio.activities;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.DialogFragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.darksoft.minegocio.R;
-import com.darksoft.minegocio.adapters.AdapterNegocio;
 import com.darksoft.minegocio.databinding.ActivityMainBinding;
-import com.darksoft.minegocio.dialogs.PopUpNuevoGasto;
-import com.darksoft.minegocio.dialogs.PopUpNuevoIngreso;
-import com.darksoft.minegocio.models.NegocioModel;
-import com.darksoft.minegocio.utilities.FechaActual;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
-    private FechaActual fechaActual = new FechaActual();
-
     private AppBarConfiguration mAppBarConfiguration;
-    private RecyclerView listaVentas;
-    private AlertDialog dialog;
-    private AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = binding.drawerlayout;
         NavigationView navigationView = binding.navigationView;
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_calendar)
+                R.id.nav_home, R.id.nav_calendar, R.id.nav_perfil)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.main);
